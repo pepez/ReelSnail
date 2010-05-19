733cc5b6b7915d188ed1ae707dc29e5d3fdbedba
pepez <petteri.hietavirta@gmail.com>
2010-05-19 20:15:52 +0100

    Minor change on license text. Code clean up

--- Atom.m
+++ Atom.m
@@ -1,7 +1,9 @@
 // ReelSnail
 // Copyright (C) 2010  Petteri Hietavirta
 //
-// This program is free software: you can redistribute it and/or modify
+// This file is part of ReelSnail.
+// 
+// ReelSnail is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
@@ -16,7 +18,6 @@
 //
 // reelsnail@gmail.com
 //
-
 
 #import "Atom.h"
 
